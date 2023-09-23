49121af1ed938f7d80ecf7fed4ca3da262d1bd97
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-09-23 18:59:25 +0500

    Updated main.js
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

--- EAGLER WEB DEV CONSOLE/main.js
+++ EAGLER WEB DEV CONSOLE/main.js
@@ -1,1 +1,45 @@
+        const openConsoleButton = document.getElementById("openConsoleButton");
+        const closeConsoleButton = document.getElementById("closeConsoleButton");
+        const consoleModal = document.getElementById("consoleModal");
+        const consoleDiv = document.getElementById("console");
+        const input = document.getElementById("input");
 
+        // Function to display a message in the console
+        function writeToConsole(message) {
+            consoleDiv.innerHTML += message + "<br>";
+            consoleDiv.scrollTop = consoleDiv.scrollHeight;
+        }
+
+        // Function to handle user input
+        function handleInput() {
+            const command = input.value.trim();
+            input.value = "";
+
+            // Display the entered command in the console
+            writeToConsole(input.placeholder + command);
+
+            writeToConsole("Command not recognized: " + command);
+        }
+
+        // Handle Enter key press in the input field
+        input.addEventListener("keydown", function(event) {
+            if (event.key === "Enter") {
+                handleInput();
+            }
+        });
+
+        // Open the console when the button is clicked
+        openConsoleButton.addEventListener("click", function() {
+            consoleModal.style.display = "block";
+        });
+
+        // Close the console when the close button is clicked
+        closeConsoleButton.addEventListener("click", function() {
+            consoleModal.style.display = "none";
+        });
+
+        writeToConsole("Eaglercraft by EaglerDevs [Version 0.0.1-alpha]");
+        writeToConsole("(c) Eagler Devs. All rights reserved.");
+        writeToConsole("");
+        writeToConsole("EAGLER:\\> Welcome to the Web Dev Console");
+        writeToConsole("EAGLER:\\> Type 'help' for a list of commands.");
