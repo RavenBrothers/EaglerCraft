2129d4f32a45379ce6765a6811d9b10506cc7fef
FlameV3 <Flameclient1.0@gmail.com>
2023-09-30 04:49:56 -0400

    Betterd By FlamePVP
    Signed-off-by: FlameV3 <Flameclient1.0@gmail.com>

--- EaglerCraftLauncher.py
+++ EaglerCraftLauncher.py
@@ -1,8 +1,6 @@
 import webview
 import tkinter as tk
 from tkinter import *
-from tkinter import ttk
-from ttkthemes import ThemedStyle
 
 # Create the main application window
 root = tk.Tk()
@@ -32,12 +30,8 @@ clients = {
     'Eaglercraft 1.14': 'https://eaglerdevs.github.io/EaglerCraft/'
 }
 
-# Create a ThemedStyle for ttk widgets
-style = ThemedStyle(root)
-style.set_theme("plastik")  # You can choose different themes
-
 # Create a label for the title with a transition effect
-title_label = ttk.Label(root, text='Welcome to the Eaglercraft Launcher', font=("Arial", 20, "bold"))
+title_label = Label(root, text='Welcome to the Eaglercraft Launcher', font=("Arial", 20, "bold"))
 title_label.pack(pady=20)
 title_label.configure(foreground="blue")  # Initial color
 
@@ -47,14 +41,15 @@ def change_title_color():
 root.after(2000, change_title_color)  # Change color after 2 seconds (adjust timing as needed)
 
 # Create a label for client selection
-client_label = ttk.Label(root, text='Select a Eaglercraft Client:', font=("Arial", 14))
+client_label = Label(root, text='Select a Eaglercraft Client:', font=("Arial", 14))
 client_label.pack()
 
 # Create a dropdown menu with a transition effect
 clicked = StringVar()
 clicked.set(options[0])
 
-drop = ttk.Combobox(root, textvariable=clicked, values=options, font=("Arial", 12))
+drop = OptionMenu(root, clicked, *options)
+drop.config(font=("Arial", 12))
 drop.pack(pady=10)
 
 # Function to launch the selected client with a transition effect
@@ -67,11 +62,11 @@ def launch():
         webview.start()
     else:
         # Display an error message if "Choose Eaglercraft Client" is selected
-        error_label = ttk.Label(root, text="Please select a valid Eaglercraft client.", foreground="red", font=("Arial", 12))
+        error_label = Label(root, text="Please select a valid Eaglercraft client.", foreground="red", font=("Arial", 12))
         error_label.pack(pady=10)
 
 # Create a launch button with a transition effect
-launch_button = ttk.Button(root, text="Launch Eaglercraft Client", command=launch, style='TButton')
+launch_button = Button(root, text="Launch Eaglercraft Client", command=launch, font=("Arial", 16))
 launch_button.pack(pady=20)
 
 # Function to exit the application with a transition effect
@@ -79,7 +74,7 @@ def exit_launcher():
     root.destroy()
 
 # Create an Exit button with a transition effect
-exit_button = ttk.Button(root, text="Exit", command=exit_launcher, style='TButton')
+exit_button = Button(root, text="Exit", command=exit_launcher, font=("Arial", 16))
 exit_button.pack(pady=10)
 
 # Run the main application loop
