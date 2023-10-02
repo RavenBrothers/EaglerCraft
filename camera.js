48ae4ba94d3ba89da29e9192ee26150c03b6b1a6
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-10-02 18:33:28 +0500

    Create camera.js
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

---
+++ camera.js
@@ -0,0 +1,10 @@
+const camera = document.querySelector('.camera');
+let cameraX = 0;
+
+function animateCamera() {
+    cameraX += 1; // Adjust the speed of the camera movement here
+    camera.style.transform = `translateX(${cameraX}px)`;
+    requestAnimationFrame(animateCamera);
+}
+
+animateCamera();
