f72d67cfdd9fd1b6ab51ab6a484f4c008e172952
FlameV3 <Flameclient1.0@gmail.com>
2023-10-02 12:18:46 -0400

    Update python_demo.py
    Signed-off-by: FlameV3 <Flameclient1.0@gmail.com>

--- GUIDE/python_demo.py
+++ GUIDE/python_demo.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 
-#code imporved by btplayzx
-
+# Improved By FlamePVP
 app = Flask(__name__)
 
+# Define the index route
 @app.route('/')
 def index():
-  return render_template('index.html')
+    return render_template('index.html')
 
-if __name__ == 'main':
-  app.run(debug=True)
+# Check if this script is executed directly and run the app
+if __name__ == '__main__':
+    app.run(debug=True)
