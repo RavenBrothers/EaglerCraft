7bf75b8d7e037179706dc30096b142661cb1013a
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-10-02 18:42:17 +0500

    Update camera.js
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

--- camera.js
+++ camera.js
@@ -2,9 +2,9 @@ const camera = document.querySelector('.camera');
 let cameraX = 0;
 
 function animateCamera() {
-    cameraX += 1; // Adjust the speed of the camera movement here
-    camera.style.transform = `translateX(${cameraX}px)`;
-    requestAnimationFrame(animateCamera);
+   cameraX += 1; // Adjust the speed of the camera movement here
+   camera.style.transform = `translateX(${cameraX}px)`;
+   requestAnimationFrame(animateCamera);
 }
 
 animateCamera();
