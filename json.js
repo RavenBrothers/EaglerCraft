99959d04d11c5d89d2745cd29f511dde11139a19
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-10-04 11:56:23 +0500

    Update json.js
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

--- json.js
+++ json.js
@@ -12,9 +12,7 @@ async function fetchAndDisplayJSON() {
         const jsonContent = document.getElementById('json-content');
         jsonContent.textContent = JSON.stringify(jsonData, null, 2); // Format the JSON for better readability
     } catch (error) {
- console.error('An error occurred:', error.message);
-
-        console.error(error.stack);
+ console.error('An error occurred!', error.message);
     }
 }
 
