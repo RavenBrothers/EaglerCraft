1a3cc7bd1ff482f623c479db1da5bd6654f38271
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-10-06 15:47:22 +0500

    Rename to assets.js and do something
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

---
+++ assets.js
@@ -0,0 +1,6 @@
+const jsonData = {
+assetName: 'https://raw.githubusercontent.com/EaglerDevs/EaglerCraft/main/';
+assetName: 'https://github.com/EaglerDevs/Eaglercraft/';
+}
+
+export default jsonData;
