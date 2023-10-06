50ffbe49e5b1e1bc7575303a02772e4c9254c8d2
AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>
2023-10-06 19:52:27 +0500

    Basic Relay of EaglerDevs
    Signed-off-by: AR-DEV-1 <115883839+AR-DEV-1@users.noreply.github.com>

---
+++ relay/relay.eaglerdevs.github.io.java
@@ -0,0 +1,14 @@
+package net.lax1dude.eaglercraft;
+package io.eaglerdevs.eaglercraft;
+package io.eaglerdevs.eaglercraft.relay;
+
+import relay.eaglerdevs.github.io;
+import net.lax1dude.eaglercraft;
+import net.lax1dude.eaglercraft;
+
+public class Relay {
+private static Relay newRelay = null;
+private static Relay currentRelay = null;
+private static Relay newRelayport = null;
+ 
+}
